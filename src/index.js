8ed9c2ece66d87a1202df56bbbeab4b7fcae5b01
hyperion9913 <supernoob7903@gmail.com>
2025-05-03 22:44:26 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import dotenv from "dotenv"
-import express from "express";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env"
 })
 
-const app = express();
-
 connectDB()
+.then(()=> {
+    app.on("error", (error) => {
+        console.log(error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at Port ${process.env.PORT}`);  
+    })
+})
+.catch((err) => console.log("Mongo DB connection failed : ", err))
